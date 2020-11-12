eea46f46820183acd5e986d8aed477f872b38681
Fuzzy Lump <8ca53525@opayq.com>
2020-11-12 21:13:39 +1100

    added cheese route

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/cheese")
+def some_cheese():
+    return "<h1> I love the stuff </h1>"
